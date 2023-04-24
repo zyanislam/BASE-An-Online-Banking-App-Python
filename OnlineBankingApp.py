559fc982f0939fcba3bf0e77d370912dd8fc5e9a
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-04-24 14:39:58 +0600

    Update OnlineBankingApp.py

--- OnlineBankingApp.py
+++ OnlineBankingApp.py
@@ -1,1 +1,10 @@
 import math
+print("Welcome to BASE: An Online Banking Application")
+
+
+def signup():
+    global name
+    global pin
+    global currentBalance
+    name = str(input("Enter an username"))
+    pin = str(input("Enter your 6 digit PIN"))
